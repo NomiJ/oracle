d914fcecd913eff4058f6d1f290fe470906aa59c
Nomi <itsnomihere@gmail.com>
2021-02-21 03:43:51 +1100

    move contract inside contract folder

--- oracle/contracts/EthPriceOracle.sol
+++ oracle/contracts/EthPriceOracle.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.7.4;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "../caller/EthPriceOracleInterface.sol";
+import "../../caller/contracts/EthPriceOracleInterface.sol";
 import "./CallerContracInterface.sol";
 
 
