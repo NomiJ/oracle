ad8c6c6b052c36d560b6cdeb2ab4a8944ea28ee2
Nomi <itsnomihere@gmail.com>
2021-02-21 03:19:34 +1100

    Fix contract location + private file add

--- js/client.js
+++ js/client.js
@@ -1,8 +1,8 @@
 const common = require('./utils/common.js')
 const SLEEP_INTERVAL = process.env.SLEEP_INTERVAL || 2000
 const PRIVATE_KEY_FILE_NAME = process.env.PRIVATE_KEY_FILE || '../caller/caller_private_key'
-const CallerJSON = require('../caller/build/contracts/CallerContract.json')
-const OracleJSON = require('../oracle/build/contracts/EthPriceOracle.json')
+const CallerJSON = require('../bin/caller/CallerContract.json')
+const OracleJSON = require('../bin/oracle/EthPriceOracle.json')
 
 async function getCallerContract(web3js) {
     const networkId = await web3js.eth.net.getId()
@@ -22,6 +22,7 @@ async function retrieveLatestEthPrice() {
 
 async function filterEvents(callerContract) {
     callerContract.events.PriceUpdatedEvent({ filter: {} }, async(err, event) => {
+        ls
         if (err) console.error('Error on event', err)
         console.log('* New PriceUpdated event. ethPrice: ' + event.returnValues.ethPrice)
     })
@@ -38,16 +39,21 @@ async function init() {
 }
 
 (async() => {
-    const { callerContract, ownerAddress, client, web3js } = await init()
-    process.on('SIGINT', () => {
-        console.log('Calling client.disconnect()')
-        client.disconnect();
-        process.exit();
-    })
-    const networkId = await web3js.eth.net.getId()
-    const oracleAddress = OracleJSON.networks[networkId].address
-    await callerContract.methods.setOracleInstanceAddress(oracleAddress).send({ from: ownerAddress })
-    setInterval(async() => {
-        await callerContract.methods.updateEthPrice().send({ from: ownerAddress })
-    }, SLEEP_INTERVAL);
+    try {
+        console.log('Starting client... ')
+        const { callerContract, ownerAddress, client, web3js } = await init()
+        process.on('SIGINT', () => {
+            console.log('Calling client.disconnect()')
+            client.disconnect();
+            process.exit();
+        })
+        const networkId = await web3js.eth.net.getId()
+        const oracleAddress = OracleJSON.networks[networkId].address
+        await callerContract.methods.setOracleInstanceAddress(oracleAddress).send({ from: ownerAddress })
+        setInterval(async() => {
+            await callerContract.methods.updateEthPrice().send({ from: ownerAddress })
+        }, SLEEP_INTERVAL);
+    } catch (e) {
+        console.log('This is e ' + e)
+    }
 })()
